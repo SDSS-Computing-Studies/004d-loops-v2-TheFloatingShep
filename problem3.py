bd97fabb12b90d6aef8a06d57cfa796646cbb592
Kenji MacMartin <47537573+TheFloatingShep@users.noreply.github.com>
2020-10-02 12:42:10 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -1,7 +1,6 @@
 #! python3
 n = int(input("GIMME A NOMBERE\n"))
-b = n + 1
 a = 0
-for x in range(1,b):
+for x in range(1,n+1):
     a = a + int("1" * x)
 print("the sum of the series is " + str(a))
